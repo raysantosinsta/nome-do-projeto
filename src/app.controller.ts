76d5dbbdd7f4f7c1be90d904f8239f7a6ecd090f
Ray Santos <raysantosinsta@gmail.com>
2026-03-04 15:50:13 -0300

    new

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+/* eslint-disable @typescript-eslint/require-await */
+import { Controller, Post, Body, Res } from '@nestjs/common';
+import type { Response } from 'express';
+import * as twilio from 'twilio';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+@Controller('webhook/twilio')
+export class TwilioController {
+  @Post()
+  async receive(@Body() body: any, @Res() res: Response) {
+    console.log('BODY:', body);
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+    const twiml = new twilio.twiml.MessagingResponse();
+
+    twiml.message('Recebi sua mensagem 🔥');
+
+    res.type('text/xml');
+    return res.send(twiml.toString());
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(express.urlencoded({ extended: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
