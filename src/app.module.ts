36d9deb6cc2f3aa86598f0cec01acb47ccf4e3fb
Ray Santos <raysantosinsta@gmail.com>
2026-03-04 15:54:23 -0300

    cnr

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { TwilioController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [TwilioController],
   providers: [AppService],
 })
 export class AppModule {}
